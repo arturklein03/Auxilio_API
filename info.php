c031f841a5651a513bcbf01be91cb4687924640b
Martin <96021106+MartinGrings@users.noreply.github.com>
2022-09-30 23:23:17 -0300

    Add files via upload

--- info.php
+++ info.php
@@ -14,7 +14,16 @@
     <div>
         <div class="titulo"><h2>Objetivos</h2></div>
         <div class="paragrafo">
-            <p>O projeto tem por objetivo analisar os dados disponibilizados pelo governo federal sobre o Auxílio Brasil</p>
+            <p>O projeto tem por objetivo analisar os dados disponibilizados pelo governo federal sobre o Auxílio Brasil e observar quais foram as datas dos saques e os valores recebidos pelas pessoas, podendo assim verificar quanto dinheiro foi investido em cada município e as diferenças entre municípios.</p>
+        </div>
+        <div class = "titulo"><h2>Procedimentos Metodológicos</h2></div>
+        <div>
+            <p class = "paragrafo">Foi utilizado uma API disponibilizada pelo o governo federal para a obtenção dos dados, o objetivo inicial era disponibilizar os dados de todas cidades de Brasil, porém, por causa de uma limitação da API, não foi possível obter os dados de cidades com muitos habitantes, por isso limitamos o projeto para apenas cidades do Vale do Caí.</p>
+            <p class = "paragrafo">Para a utilização da API é necessário uma chave de acesso que é disponibilizada gratuitamente, o código do IBGE da cidade que você procura e o mês que você quer os dados sobre, estão disponíveis os dados de Novembro de 2021 até Julho de 2022. A resposta dessa pesquisa é processada e os resultados impressos na tela, em forma de gráfico e números informando os dados sobre o município escolhido.</p>
+        </div>
+        <div class = "titulo"><h2>Conclusão</h2></div>
+        <div>
+            <p class = "paragrafo">Infelizmente por causa na limitação da API não conseguimos mostrar os dados de qualquer cidade de país, mas para cidades do Vale do Caí conseguimos demonstrar a quantidade de recursos dedicados à sua população, e podemos perceber que o valor médio distribuído nas cidades e geralmente similar entre todas elas, e as datas que são realizadas os saques do benefício nas cidades são geralmente distribuídas quase igualmente.</p>
         </div>
     </div>
     <a class="titulo" href="index.php"><h3>Voltar</h3></a>
